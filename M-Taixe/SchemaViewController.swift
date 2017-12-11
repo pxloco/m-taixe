70910fb8d473730706db0a240331b09e21a75576
locpx <locpx@vzota.com.vn>
2017-12-11 22:11:55 +0700

    Fix Bug Collection View

---
+++ M-Taixe/SchemaViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SchemaViewController.swift
+//  M-Taixe
+//
+//  Created by M on 12/11/17.
+//  Copyright © 2017 kha. All rights reserved.
+//
+
+import Foundation
---
+++ M-Taixe/SeatView.swift
@@ -0,0 +1,9 @@
+//
+//  Seat.swift
+//  M-Taixe
+//
+//  Created by M on 12/10/17.
+//  Copyright © 2017 kha. All rights reserved.
+//
+
+import Foundation
